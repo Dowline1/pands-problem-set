ebbe257bffd6837e17d9022d9228276512766e15
Eoin Dowling <eoindowling505@gmail.com>
2019-02-27 14:43:30 +0000

    Solution 3 completed

--- Solution-3.py
+++ Solution-3.py
@@ -2,11 +2,13 @@
 # Eoin Dowling 27-Feb-2019
 
 # My Assumption of between in problem 3 is 1001....9999 inclusive. 
-Start = 1001
+Start = 1000
+End = 10000
 
-#Started code using understanding of While loops from Solution 1.
-while Start < 10000:
-	Result= Start/6
-	print (Result)
-	Start = Start + 1
+# Switched to for statement to so that could use range function, code adapted from: https://www.sanfoundry.com/python-program-print-numbers-range-divisible-given-number/
+# Range includes all number between 2 points ie not including two numbers, lists can also be used
+for i in range(Start, End+1):
+	  #as described in previous lectures modulo (%) returns remainder of division of 2 numbers eg 2%6 = 2 as 2 is remainder as 6 does not divide into 2.
+		if(i%6==0):
+			print(i)
 
