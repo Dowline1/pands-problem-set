88041ab122aaa69976802f1180976b374d371ce2
Eoin Dowling <eoindowling505@gmail.com>
2019-02-27 12:14:27 +0000

    Updated as per comments to give countdown of next T day

--- Solution-2.py
+++ Solution-2.py
@@ -6,13 +6,20 @@
 import datetime
 
 # Updated or statement as before did not work as did not specify all statement alternative of or.
+# Correct use of or adapted from: https://www.w3schools.com/python/python_conditions.asp
 if datetime.datetime.today().weekday() == 1 or datetime.datetime.today().weekday() == 3 :
   print("Yes, Today does begin with a T.")
 else:
   print("No, Today does not begin with T.")
 
-x = datetime.datetime.today().weekday()
-
-print(x) 
+# Adding day counter to inform how many days until it will be a day begining with T.
+if datetime.datetime.today().weekday() == 0 or datetime.datetime.today().weekday() == 2:
+  print("Tomorrow will begin with a T.")
+elif datetime.datetime.today().weekday() == 4:
+  print("Four more sleeps to go.")
+elif datetime.datetime.today().weekday() == 5:
+  print ("Three more sleeps to go.")
+elif datetime.datetime.today().weekday() == 6:
+  print("Two more sleeps to go.")
 
 
