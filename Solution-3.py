fb1bc5dd189a2f1dc5a15b75416eec882ba6ec73
Eoin Dowling <eoindowling505@gmail.com>
2019-02-27 13:26:15 +0000

    First commit Solution 3

--- Solution-3.py
+++ Solution-3.py
@@ -1,3 +1,12 @@
 # Solution to Problem 3
 # Eoin Dowling 27-Feb-2019
 
+# My Assumption of between in problem 3 is 1001....9999 inclusive. 
+Start = 1001
+
+#Started code using understanding of While loops from Solution 1.
+while Start < 10000:
+	Result= Start/6
+	print (Result)
+	Start = Start + 1
+
