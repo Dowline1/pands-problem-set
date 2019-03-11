8d54abe7a88ebff7ed651bbbbbb2c956f7aa9666
Eoin Dowling <eoindowling505@gmail.com>
2019-03-11 13:04:34 +0000

    Started Solution-6.py

---
+++ Solution-6.py
@@ -0,0 +1,7 @@
+# Solution to Problem 6
+# Eoin Dowling 11-Mar-2019
+
+# Starting program input from code created in solution 1 and adapting as necessary.
+start = int(input("Please enter a sentence "))
+
+print(start)
